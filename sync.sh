44f5491a66239d115c3021485ec0bb820ba3c2f4
yxm <yxm.dev@gmx.ie>
2023-06-05 18:37:08 -0300

    ...

--- sync.sh
+++ sync.sh
@@ -23,10 +23,23 @@ installdir="$HOME/.config/sync.sh"
     declare -A alias_git
 ## including data
     source  $installdir/cfile
+## defining inverse arrays
+    declare -A git_remote_name
+    for s in ${git_remote[@]}; do
+        for i in ${!git_dir[@]}; do
+            for (( j=0; j<=${Ngit_dir[$i]}; j++ )); do
+                if [[ "${git_remote[$i,$j]}" == "$s" ]]; then
+                    git_remote_name[$i,$j]="ok"
+                else
+                    git_remote_name[$i,$j]=""
+                fi
+            done
+        done
+    done
 
 # SYNC
     function sync() {
-## defining auxiliary functions
+## Auxiliary Functions
         function sync_core(){
             echo "Synchronizing \"$1\" with \"$2\"..."
             sudo rsync -av --progress --delete --exclude '.git/*' --exclude 'README.md' $1 $2
@@ -76,7 +89,8 @@ installdir="$HOME/.config/sync.sh"
             git push $3  $4
             echo "Done!"
         }
-## sync function propertly
+## SYNC Function Propertly
+### local-local sync from aliases
             for i in ${!alias_dir[@]}; do
                 if [[ "$1" == "${alias_dir[$i]}" ]]; then
                     sync_core ${source_dir[$i]} ${target_dir[$i]}
@@ -89,40 +103,69 @@ installdir="$HOME/.config/sync.sh"
                     fi
                 done
             done
+### local-git sync from git initialized dir name 
             for i in ${!git_dir[@]}; do
-                if [[ "$1" == "--git" ]] || [[ "$1" == "-g" ]] && 
-                   [[ "$2" == "$i" ]] || [[ "$2" == "${git_dir[$i]}" ]]; then
+                if ([[ "$1" == "--git" ]] || [[ "$1" == "-g" ]]) && 
+                   ([[ "$2" == "$i" ]] || [[ "$2" == "${git_dir[$i]}" ]]); then
                      for (( j=0; j<=${Ngit_st[$i]}; j++ )); do
                          sync_dir_file ${source_dir_git[$i,$j]} ${target_dir_git[$i,$j]}
                     done
                 fi
             done
+### push from git initialized dir name
             for i in ${!git_dir[@]}; do
-                if [[ "$1" == "--push" ]] || [[ "$1" == "-p" ]] && [[ "$2" == "$i" ]] || [[ "$2" == "${git_dir[$i]}" ]]; then
+                if ([[ "$1" == "--push" ]] || [[ "$1" == "-p" ]]) && 
+                    ([[ "$2" == "$i" ]] || [[ "$2" == "${git_dir[$i]}" ]]); then
                     for (( j=0; j<=${Ngit_st[$i]}; j++ )); do
                         sync_push ${git_init_dir[$i,$j]} ${git_commit[$i,$j]} ${git_remote[$i,$j]} ${git_branch[$i,$j]}
                     done
                 fi
             done
+### push from git initialized dir name AND remote 
+            for i in ${!git_dir[@]}; do
+                for (( j=0; j<=${Ngit_dir[$i]}; j++ )); do
+                    if ([[ "$1" == "--push" ]] || [[ "$1" == "-p" ]]) && 
+                       ([[ "$2" == "$i" ]] || [[ "$2" == "$i" ]]) &&
+                       ([[ "$3" == "--remote" ]] || [[ "$3" == "-r" ]]) &&
+                       ([[ "$3" == "$j" ]] || [[ "$3" == "${git_remote[$i,$j]}" ]]); then
+                        sync_push ${git_init_dir[$i,$j]} ${git_commit[$i,$j]} ${git_remote[$i,$j]}  ${git_branch[$i,$j]}
+                    fi
+                done
+            done
+### push from remote
+            for s in ${git_remote[@]}; do
+                for i in ${!git_dir[@]}; do
+                    for (( j=0; j<=${Ngit_dir[$i]}; j++ )); do
+                        if ( ( ([[ "$1" == "--push" ]] || [[ "$1" == "-p" ]]) &&
+                            ([[ "$2" == "--remote" ]] || [[ "$2" == "-r" ]]) ) && 
+                            [[ "$3" == "$s" ]] ) ||
+                            ([[ "$1" == "-pr" ]] && [[ "$2" == "$s" ]]); then
+                                if [[ "$s" == "${git_remote[$i,$j]}" ]]; then
+                                    sync_push ${git_init_dir[$i,$j]} ${git_commit[$i,$j]} ${git_remote[$i,$j]} ${git_branch[$i,$j]}
+                                fi
+                        fi
+                    done
+                done
+            done
+            
+### push from branch
+
+### clean temporary files
             if [[ "$1" == "--clean" ]] || [[ "$1" == "-cl" ]]; then
                 echo "cleaning temporary files..."
                 find $HOME -type f -iname ".*.*.??????" -delete
                 echo "done!"
+### local-local sync all dirs
             elif [[ "$1" == "--dirs" ]] || [[ "$1" == "-d" ]]; then
                 for j in ${!alias_dir[@]}; do
                     sync_dir_file ${source_dir[$j]} ${target_dir[$j]}
                 done
+### local-local sync all files
             elif [[ "$1" == "--files" ]] || [[ "$1" == "-f" ]]; then
                 for k in  ${!source_file[@]}; do
                     sync_dir_file ${source_file[$k]} ${target_file[$k]}
                 done
-            elif [[ "$1" == "--git" ]] || [[ "$1" == "-g" ]] && [[ -z $2 ]]; then
-                for i in ${!git_dir[@]}; do
-                     for (( j=0; j<=${Ngit_st[$i]}; j++ )); do
-                        sync_dir_file ${source_dir_git[$i,$j]} ${target_dir_git[$i,$j]}
-                    done
-                done
-
+### initialize git dirs
             elif [[ "$1" == "--git-init" ]] ||
                  [[ "$1" == "-gi" ]] || 
                  [[ "$1" == "--git" ]] && [[ "$2" == "--init" ]] ||
@@ -158,7 +201,14 @@ installdir="$HOME/.config/sync.sh"
                         fi
                     done
                 done
-
+### local-git sync all git initialized dirs
+            elif [[ "$1" == "--git" ]] || [[ "$1" == "-g" ]] && [[ -z $2 ]]; then
+                for i in ${!git_dir[@]}; do
+                     for (( j=0; j<=${Ngit_st[$i]}; j++ )); do
+                        sync_dir_file ${source_dir_git[$i,$j]} ${target_dir_git[$i,$j]}
+                    done
+                done
+### full local-local and local-git sync           
             elif [[ "$1" == "--all" ]] || [[ "$1" == "-a" ]]; then
                 sync -cl
                 sync -f 
@@ -170,11 +220,13 @@ installdir="$HOME/.config/sync.sh"
                          sync_push ${git_init_dir[$i,$j]} ${git_commit[$i,$j]} ${git_remote[$i,$j]}  ${git_branch[$i,$j]}
                     done
                 done
+### start configuration
             elif [[ "$1" == "--config" ]] || [[ "$1" == "-c" ]]; then
                     echo "executing the config file..."
+### display help
             elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
                     echo "showing help..."
-
+### display error
             else
                     echo "option not defined for the \"sync\" function."
             fi
